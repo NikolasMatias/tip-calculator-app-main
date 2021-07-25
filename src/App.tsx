1564f4a50cfacc5fb05df9bffea943e568c3a89a
Nikolas Matias <nikolas.matias500@gmail.com>
2021-07-25 07:57:53 -0300

    Starting my basic scss and icons for the index.html file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className="tip-calculator">
+
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
