fcca15a82473300c82ece2b2219b9c26ff6c43a1
Nikolas Matias <nikolas.matias500@gmail.com>
2021-07-25 23:54:51 -0300

    Mobile Design finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import logo from './assets/images/logo.svg';
 import './App.scss';
+import TipsDefault from "./helpers/TipsDefault";
 
 function App() {
+    const [bill, setBill] = useState<number>(0);
+    const [numberOfPeople, setNumberOfPeople] = useState<number>(0);
+
   return (
     <div className="tip-calculator">
-
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="title">
+          <img src={logo} className="title-logo" alt="Website logo"/>
+        </div>
+        <div className="content">
+            <div className="bill">
+              <label htmlFor="bill" className="label-bill">Bill</label>
+              <input type="text" name="bill" id="bill" value={bill} onChange={(e) => {setBill(e.target.value.length > 0 ? parseFloat(e.target.value) : 0)}}/>
+            </div>
+            <div className="tip">
+                <label>Select Tip %</label>
+              {TipsDefault.map((tip, index, array) => (
+                  <button key={index} type="button" className="btn-tip">{`${tip.value}%`}</button>
+              ))}
+              <input type="text" name="tip-custom" placeholder="Custom" className="input-tip-custom"/>
+            </div>
+            <div className="number-of-people">
+              <label htmlFor="number-of-people" className="label-number-of-people">Number of People</label>
+              <input type="text" name="number-of-people" id="number-of-people" value={numberOfPeople} onChange={(e) => {setNumberOfPeople(e.target.value.length > 0 ? parseFloat(e.target.value) : 0)}}/>
+            </div>
+            <div className="results">
+              <div className="results-values">
+                  <div className="tip-per-person">
+                      <div className="title-tip-per-person">
+                          <h3>Tip Amount</h3>
+                      </div>
+                      <p className="value-tip-per-person">$4.27</p>
+                  </div>
+                  <div className="total-per-person">
+                      <div className="title-total-per-person">
+                          <h3>Total</h3>
+                      </div>
+                      <p className="value-total-per-person">$32.79</p>
+                  </div>
+              </div>
+                <button className="results-reset">RESET</button>
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/helpers/TipsDefault.ts
@@ -0,0 +1,13 @@
+export interface ITipsDefualt {
+    value: number
+}
+
+const TipsDefault: ITipsDefualt[] = [
+    {value: 5},
+    {value: 10},
+    {value: 15},
+    {value: 25},
+    {value: 50}
+];
+
+export default TipsDefault;
